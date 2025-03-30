7e1aa7fe25a6721eda0fe4f88f3d0fe5862d1b40
Xinyi Chen <21XChen@heckgrammar.co.uk>
2025-03-30 20:07:24 +0100

    ghn

--- ShapeAreaCalculatorTask/Program.cs
+++ ShapeAreaCalculatorTask/Program.cs
@@ -11,9 +11,31 @@
             // Pentagon
             // Octagon
         }
-        static int areaOfRectangle()//Add the parameters
-        {
 
+        static double areaOfCircle(double radius)
+        {
+            return radius * radius * Math.PI;
         }
+
+        static double areaOfTriangle(double width, double height)
+        {
+            return (width * height) / 2;
+        }
+
+        static double areaOfRectangle(double width, double length)
+        {
+            return width * length;
+        }
+
+        static double areaOfPentagon(double Base, double perpendicularHeight)
+        { 
+            return 5/2 * Base * perpendicularHeight;
+        }
+
+        static double areaOfOctogon(double sideLength)
+        { 
+            return 2 * (1 + Math.Sqrt(2)) * sideLength * sideLength;
+        }
+
     }
 }
